515f1063fd1fe5441d7783ce9b2d485d89819867
risami27 <rishmie123@gmail.com>
2024-03-23 11:05:58 -0700

    final commit

--- ACTIVITY_TWO/js/app.js
+++ ACTIVITY_TWO/js/app.js
@@ -8,13 +8,13 @@ let b = document.getElementById("sectionTwo")
 let Obj2={
     title2: "BABIES",
     img1: "../image/ruffruner.jpg",
-    desc1: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam, est ratione aut veritatis placeat earum voluptatum blanditiis officiis soluta sunt quod atque modi dolor quibusdam, at illum commodi aspernatur quos.",
+    desc1: " Ruffrunner is easily the least curious and adventurous of the Night Lights. When not play-fighting with his brother Pouncer, he loves nothing more than to find a sunny spot to curl up and take a nap.",
     btn1: "Ruffruner",
     img2: "../image/pouncer.jpg",
-    desc2: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam, est ratione aut veritatis placeat earum voluptatum blanditiis officiis soluta sunt quod atque modi dolor quibusdam, at illum commodi aspernatur quos.",
+    desc2: "Pouncer can create blinding flash of fire to distract his enemies. The ability appears to be unique to him.",
     btn2: "Pouncer",
     img3: "../image/dart.jpg",
-    desc3: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam, est ratione aut veritatis placeat earum voluptatum blanditiis officiis soluta sunt quod atque modi dolor quibusdam, at illum commodi aspernatur quos.",
+    desc3: "The only female among the siblings, she has black as her predominant body color, while the medial and posterior areas are white, faintly tinged in pink.",
     btn3: "Dart"
 }
 
